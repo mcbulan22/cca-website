6f641398fad42db5d80bd7f4d4f323c96e13b901
mcbulan22 <113360257+mcbulan22@users.noreply.github.com>
2025-05-21 01:57:28 +0000

    edit tab title

--- src/pages/manifest.json.ts
+++ src/pages/manifest.json.ts
@@ -44,8 +44,8 @@ export const GET: APIRoute = async () => {
   );
 
   const manifest = {
-    short_name: "ScrewFast",
-    name: "ScrewFast",
+    short_name: "CCA",
+    name: "Center for Competency Assessment",
     icons,
     display: "minimal-ui",
     id: "/",
