2d3772f4c94cbb113531646e265b436619fc41e6
mcbulan22 <113360257+mcbulan22@users.noreply.github.com>
2025-05-24 05:24:49 +0000

    put logo in public/image

--- src/components/ui/icons/icons.ts
+++ src/components/ui/icons/icons.ts
@@ -16,7 +16,8 @@ export const Icons = {
         d: "M343-420h225v-60H343v60Zm0-90h395v-60H343v60Zm0-90h395v-60H343v60Zm-83 400q-24 0-42-18t-18-42v-560q0-24 18-42t42-18h560q24 0 42 18t18 42v560q0 24-18 42t-42 18H260Zm0-60h560v-560H260v560ZM140-80q-24 0-42-18t-18-42v-620h60v620h620v60H140Zm120-740v560-560Z",
       },
     ],
-    class: "mt-1 h-8 w-8 shrink-0 fill-orange-400 dark:fill-orange-300",
+    class:
+      "h-6 w-6 shrink-0 text-neutral-700 hs-tab-active:text-orange-400 dark:text-neutral-300 dark:hs-tab-active:text-orange-300 md:h-7 md:w-7",
     width: 48,
     height: 48,
     viewBox: "0 -960 960 960",
